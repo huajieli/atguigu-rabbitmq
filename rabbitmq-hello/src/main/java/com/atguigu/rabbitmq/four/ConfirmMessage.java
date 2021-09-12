a57ba3d9866f977045b5f6e009e30889e0b1a7b8
huajieli <lhj@08110202>
2021-09-12 19:58:33 +0800

    异步确认消息的处理

--- rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/four/ConfirmMessage.java
+++ rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/four/ConfirmMessage.java
@@ -5,6 +5,8 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.ConfirmCallback;
 
 import java.util.UUID;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * @author huajieli
@@ -15,9 +17,9 @@ public class ConfirmMessage {
 
     public static void main(String[] args) throws Exception {
         //1:单个确认
-        ConfirmMessage.publishMessageIndividually();
+        //ConfirmMessage.publishMessageIndividually();
         //2：批量确认
-        ConfirmMessage.publishMessageBatch();
+        //ConfirmMessage.publishMessageBatch();
         //3：异步确认
         ConfirmMessage.publishMessageSync(); //发送1000条耗时29ms
 
@@ -90,18 +92,35 @@ public class ConfirmMessage {
         channel.queueDeclare(queueName, true, false, false, null);
         //开启发布确认
         channel.confirmSelect();
+        /**
+         * 线程安全有序的一个哈希表（map） 适用于高并发的情况下
+         * 1:轻松的将序号和消息进行关联
+         * 2：轻松批量删除条目，只要给到序号
+         * 3：支持高并发（多线程）
+         */
+        ConcurrentSkipListMap<Long,String> outstandingConfirms = new ConcurrentSkipListMap<>();
+
         //开始时间
         long begin = System.currentTimeMillis();
 
         //监听成功的消息
         ConfirmCallback ackCallBack = (deleliveryTag, multiple) -> {
+            //2：删除成功回调确认的消息
+            if(multiple){
+                //如果是批量确认这样批量删除（一般为了防止消息丢失不会去批量删除）
+                ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirms.headMap(deleliveryTag);
+                confirmed.clear();
+            }
+            outstandingConfirms.remove(deleliveryTag);
             //deleliveryTag即为消息的标记号
             System.out.println("确认的消息:" + deleliveryTag);
         };
 
         //监听失败的消息
         ConfirmCallback nackCallBack = (deleliveryTag, multiple) -> {
-            System.out.println("未确认的消息:" + deleliveryTag);
+            //打印未确认的消息
+            String unconfirmMessage = outstandingConfirms.get(deleliveryTag);
+            System.out.println("未确认的消息:" + unconfirmMessage+deleliveryTag);
         };
 
         /**
@@ -114,6 +133,8 @@ public class ConfirmMessage {
         for (int i = 0; i < MESSAGE_COUNT; i++) {
             String message = i + "";
             channel.basicPublish("", queueName, null, message.getBytes());
+            //1：此处记录下所有要发送的消息(key：value形式)
+            outstandingConfirms.put(channel.getNextPublishSeqNo(),message);
         }
         //结束时间
         long end = System.currentTimeMillis();
